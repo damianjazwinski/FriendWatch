5afa0862f7d46819892cb4fbfe31edcf8ce252b1
Damian Jaźwiński <damian.jazwinski@nask.pl>
2023-07-04 21:05:44 +0200

    Add Watch (placeholder) entity and WatchController

---
+++ FriendWatch/Controllers/WatchController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FriendWatch.Controllers
+{
+    [Route("api/[controller]")]
+    public class WatchController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("Test");
+        }
+    }
+}
--- FriendWatch/Entities/Watch.cs
+++ FriendWatch/Entities/Watch.cs
@@ -2,5 +2,6 @@
 {
     public class Watch
     {
+        public int Id { get; set; }
     }
 }
